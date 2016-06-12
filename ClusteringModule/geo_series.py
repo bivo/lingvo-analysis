a18816291cfef154cccbb57f9cfdbe5dc37b018c
Vyacheslav Shamaev <v.shamaev73@gmail.com>
2016-06-13 00:41:18 +0400

    fix revenue data, copypaste clustering

---
+++ ClusteringModule/geo_series.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+
+
+def entry_point(filename):
+    series = read_file(filename)
+    models = build_models(series)
+    indexes = clusterize(models)
+
+    result = pd.DataFrame(data=indexes, index=series.ix[:, 0], dtype=object)
+
+    return result, models, indexes
+
+
+def build_models(series):
+    models = []
+    for _, row in series.iterrows():
+        models.append(row.values)
+
+    return models
+
+
+def clusterize(models):
+    clusters = KMeans(n_clusters=6).fit_predict(models)
+    return clusters
+
+
+def read_file(filename):
+    geo_series_frame = pd.read_csv(filename, sep=',', index_col=0)
+    return geo_series_frame
